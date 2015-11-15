6d5e67aae38e819714187314e25623a56e6dc5e8
Elvis <levigithuber@gmail.com>
2015-11-15 11:44:20 +0000

    tidying up the mess

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,4 @@
 class HomeController < ApplicationController
   def index
-    
-    @tweets = TwitterApi.get_ronaldo_tweets
-
-    respond_to do |format|
-      format.html
-      format.json { @tweets.to_json }
     end
-  end
 end
--- lib/twitter_api.rb
+++ lib/twitter_api.rb
@@ -1,19 +1,16 @@
 class TwitterApi
-    
+    #bunch of methods that help me debug my application
     def self.get_ronaldo_tweets
         client.user_timeline('Cristiano', count: 30, include_rts: false)
     end
-   
     
      def self.get_conor_tweets
         client.user_timeline('TheNotoriousMMA', count: 200, include_rts: false)
      end
     
     def self.get_user_tweets user
-        client.user_timeline(user, count: 2000, include_rts: false)
+        client.user_timeline(user.screen_name, count: 2000, include_rts: false)
     end
-        
-    
 
     def self.client
         #so that it doesn't create new object every time
@@ -21,10 +18,5 @@ class TwitterApi
           config.consumer_key        = "lXWDYfZtsiN6F8vMPpUGgQNdl"
           config.consumer_secret     = "1D9ng6XQEMy8OCruQzwY9D2hNBSJMvRu16ukmUNeVBMGGyPUWN"
         end
-        
     end
-
-
-
-    
 end
